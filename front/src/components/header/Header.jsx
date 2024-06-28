a44a4dd79dc30b357d3b8464437f3330d258db40
Pau MB <Pau MB>
2024-06-28 13:06:53 +0200

    add copy wallet

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,22 +1,56 @@
-import { Card, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+import {
+	Card,
+	Stack,
+	Typography,
+	IconButton,
+	Snackbar,
+	Alert,
+} from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 import useGetAccount from '../../hooks/web3/useGetAccount';
 import useGetBalance from '../../hooks/web3/useGetBalance';
 import Logo from '../logo/Logo';
+
+import './header.css';
+
 export default function Header() {
 	const { account, accountReduce } = useGetAccount();
 	const { balance } = useGetBalance(account);
+
+	const [open, setOpen] = useState(false);
+
+	const handleCopyClick = () => {
+		if (account) {
+			navigator.clipboard
+				.writeText(account)
+				.then(() => {
+					setOpen(true);
+				})
+				.catch(err => {
+					console.error('Error al copiar al portapapeles: ', err);
+				});
+		}
+	};
+
+	const handleClose = (_event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+		setOpen(false);
+	};
+
 	return (
 		<Stack
 			direction={'row'}
 			justifyContent={'space-between'}
-			style={{ marginBottom: '50px', marginTop: '25px' }}
+			className='header_container'
 		>
 			<Logo />
 			<Card
+				className='card'
 				sx={{
-					width: '330px',
-					height: '80px',
 					borderRadius: '20px',
 					backgroundColor: 'rgba(247, 247, 247, 0.20)',
 				}}
@@ -25,17 +59,36 @@ export default function Header() {
 					direction={'row'}
 					alignItems={'center'}
 					justifyContent={'space-around'}
-					style={{ height: '100%' }}
+					className='card_container'
 				>
 					<Stack direction={'column'}>
 						<Typography variant='body-1'>Saldo DOC</Typography>
 						<Typography variant='h6'>{Number(balance).toFixed(2)}</Typography>
 					</Stack>
 					<Stack direction={'column'}>
-						<Typography variant='body-1'>Billetera</Typography>
+						<Stack direction='row' alignItems='center'>
+							<Typography variant='body1'>Billetera</Typography>
+							<IconButton onClick={handleCopyClick} aria-label='copiar'>
+								<ContentCopyIcon sx={{ fontSize: '15px' }} />
+							</IconButton>
+						</Stack>
 						<Typography variant='h6'>
 							{accountReduce ?? 'Loading...'}
 						</Typography>
+						<Snackbar
+							open={open}
+							autoHideDuration={3000}
+							onClose={handleClose}
+							anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+						>
+							<Alert
+								onClose={handleClose}
+								severity='success'
+								className='icon_copy'
+							>
+								Wallet copiada
+							</Alert>
+						</Snackbar>
 					</Stack>
 				</Stack>
 			</Card>
